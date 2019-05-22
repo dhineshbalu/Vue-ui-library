db0a2458fece83d377137a36d79457fde7422d5f
Dhinesh Kumar <dhineshbalu97@gmail.com>
2019-05-21 19:49:40 -0700

    Add files via upload

---
+++ src/components/VueFlexCol.vue
@@ -0,0 +1,41 @@
+<template>
+<div :style="styleCustomCol">
+     <slot></slot>
+</div>
+</template>
+<script>
+export default {
+    name: 'VueFlexCol',
+    props: {
+     span: {
+         type: String
+     },
+     bgColor: {
+         type: String
+     }
+    },
+    computed: {
+      styleCustomCol() {
+          return {
+              flex: this.span ? this.span : '1',
+              flexWrap: 'wrap',
+              backgroundColor: this.bgColor,
+              padding: '20px'
+          }
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+        
+    }
+}
+</script>
+<style>
+.span1_col3 {
+}
+</style>
--- src/components/VueGridTest.vue
+++ src/components/VueGridTest.vue
@@ -8,7 +8,7 @@
                 Vue was created by Evan You after working for Google using AngularJS in a number of projects. He later summed up his thought process: "I figured, what if I could just extract the part that I really liked about Angular and build something really lightweight."[6] The first source code commit to the project was dated July 2013, and Vue was first released the following February, in 2014.
                 Vue was created by Evan You after working for Google using AngularJS in a number of projects. He later summed up his thought process: "I figured, what if I could just extract the part that I really liked about Angular and build something really lightweight."[6] The first source code commit to the project was dated July 2013, and Vue was first released the following February, in 2014.
         </VueFlexCol>
-         <VueFlexCol>
+         <VueFlexCol bgColor="green">
                Vue was created by Evan You after working for Google using AngularJS in a number of projects. He later summed up his thought process: "I figured, what if I could just extract the part that I really liked about Angular and build something really lightweight."[6] The first source code commit to the project was dated July 2013, and Vue was first released the following February, in 2014.
         </VueFlexCol>
           <VueFlexCol>
