ddace823e87a671ca98b60f560344b6a3cd65b21
Dhinesh Kumar <dhineshbalu97@gmail.com>
2019-06-01 21:04:22 -0700

    Add files via upload

---
+++ src/components/VueDropDownT.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+     <VueDropDown id="drop1" :datas="lists"></VueDropDown>
+</div>
+</template>
+<script>
+export default {
+    name: 'VueDropDownT',
+    props: {
+
+    },
+    data() {
+        return {
+            lists: [{
+                code: '1A',
+                label: 'first',
+                children: []
+            }, {
+                 code: '2A',
+                label: 'second',
+                children: [] 
+            } , {
+                   code: '3A',
+                label: 'third',
+                children: [ {
+                code: '3A1',
+                label: 'fourth',
+                children: [] 
+                }, {
+                 code: '3A2',
+                label: 'fifth',
+                children: [{
+                    code: '3A21',
+                    label:'sixth',
+                    children: [{
+                        code: '3A211',
+                        label: 'seventh',
+                        children: []
+                    }]
+                }] 
+                }] 
+            }]
+        }
+    }
+}
+</script>
+<style>
+
+</style>
